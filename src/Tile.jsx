c0c7019bb15059f011356cf4ed25c07bd7204c4c
Boluwatife Oyewande <tifeng9@gmail.com>
2024-02-21 20:56:21 +0100

    starter code

---
+++ src/Tile.jsx
@@ -0,0 +1,55 @@
+export function Tile({ content: Content, flip, state }) {
+  switch (state) {
+    case "start":
+      return (
+        <Back
+          className="inline-block h-8 w-8 bg-blue-300 text-center"
+          flip={flip}
+        />
+      );
+    case "flipped":
+      return (
+        <Front className="inline-block h-8 w-8 bg-green-500">
+          <Content
+            style={{
+              display: "inline-block",
+              width: "100%",
+              height: "100%",
+              verticalAlign: "top",
+            }}
+          />
+        </Front>
+      );
+    case "matched":
+      return (
+        <Matched className="inline-block h-8 w-8 text-gray-300">
+          <Content
+            style={{
+              display: "inline-block",
+              width: "100%",
+              height: "100%",
+              verticalAlign: "top",
+            }}
+          />
+        </Matched>
+      );
+    default:
+      throw new Error("Invalid state " + state);
+  }
+}
+
+function Back({ className, flip }) {
+  return (
+    <div onClick={flip} className={className}>
+      ?
+    </div>
+  );
+}
+
+function Front({ className, children }) {
+  return <div className={className}>{children}</div>;
+}
+
+function Matched({ className, children }) {
+  return <div className={className}>{children}</div>;
+}
